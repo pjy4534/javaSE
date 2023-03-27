815b2063d947b2b1392dceeaddeabe5530520765
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-27 20:55:39 +0900

    array

--- src/day03/loop/HomeWork2.java
+++ src/day03/loop/HomeWork2.java
@@ -14,38 +14,35 @@ public class HomeWork2 {
 			} // for j
 			System.out.println();
 		} // for i
-		
-		System.out.println();
-		
-		for(int i = 0; i < 13; i++) {
-			if(i < 10) {
-				System.out.print(" " + array);
-			}
-			System.out.print(i+": ");
-			
-			
-			
-			// 숙제코드
-			if(i<7) {
-				for(int j = 0; j < i+1; j++) {
-					if(1%2 < 1) { //인덱스번호 짝수 0포함
-						System.out.print(array[i-j][j]+" ");
-					}else {
-						System.out.print(array[i][i-j]+" ");
-					}
 
-				} else {
-					for(int j = 0; j < 13 - i; j++) {
-						if(1 % 2 < 1) {
-						System.out.print(array[6-j][i-6+j] + " ");
-					}else {
-						System.out.print(array[i-6+j][6-j] + " ");
+		System.out.println();
+
+		for (int i = 0; i < 13; i++) {
+			if (i < 10) {
+				System.out.print(" ");
+			}
+			System.out.print(i + ": ");
+
+			// 숙제코드
+			if (i < 7) {
+				for (int j = 0; j < i + 1; j++) {
+					if (i % 2 < 1) { // 인덱스번호 짝수 0포함
+						System.out.print(array[i - j][j] + " "); //array - 배열 맞추기
+					} else {
+						System.out.print(array[i][i - j] + " ");
 					}
 				}
-				
-				
+			} else {
+				for (int j = 0; j < 13 - i; j++) {
+					if (i % 2 < 1) {
+						System.out.print(array[6 - j][i - 6 + j] + " ");
+					} else {
+						System.out.print(array[i - 6 + j][6 - j] + " ");
+					}
+
+				}
 			}
-			
+
 			System.out.println();
 		}
 	} // main
