84744e228b38acaf2a5cfe104fbf4158e41a1bfa
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-04-14 21:52:10 +0900

    04.14.

--- src/day11/util/MainClass09.java
+++ src/day11/util/MainClass09.java
@@ -4,14 +4,18 @@ import java.util.Random;
 
 public class MainClass09 {
 	public static void main(String[] args) {
+		// 45크기 배열생성
 		int[] lNum = new int[45];
 		
+		// 1부터 45까지 배열이 저장
 		for(int i = 0; i< lNum.length; i++) {
 			lNum[i] = i + 1;
 		}
 		
+		// 난수생성 객체
 		Random ran = new Random();
 		for(int i = 0; i < lNum.length; i++) {
+			// 0~44까지 숫자 중 임의의 값 반환
 			int changeIndex = ran.nextInt(45);
 			
 			int tmp = lNum[i];
